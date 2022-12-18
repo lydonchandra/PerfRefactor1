8689f1a28f10899f80e0a722e1dc28f18e8c8509
Lydon Chandra <lydon.chandra@aemo.com.au>
2022-12-18 22:08:42 +0800

    multicores wip: working with 2 batches

--- Recursion/Permutation/Permutation/QuickPerm.cs
+++ Recursion/Permutation/Permutation/QuickPerm.cs
@@ -62,6 +62,61 @@ public class QuickPerm
         return result;
     }
 
+    public static List<int[]> GetPermutationsIntArrayMultiCores(int[] elementsToPermute)
+    {
+        var numCores = 2;
+        List<int[]> result = new();
+
+        int n = elementsToPermute.Length;
+        int nBatch = n / numCores;
+
+        var p = new int[n + 1];
+        for (var j = 0; j < p.Length; j++)
+            p[j] = j;
+
+        result.Add(elementsToPermute.ToArray());
+
+        var i = 1;
+
+        while (i < nBatch) // i = 1
+        {
+            p[i] -= 1; // p[1] = 1 - 1 = 0 
+            var j = 0;
+            if (i % 2 == 1) j = p[i]; // j = p[1] = 0
+
+            SwapIntArray(elementsToPermute, i, j); // swap(arr, 1, 0)
+            result.Add(elementsToPermute.ToArray());
+            i = 1; // i = 1
+            while (p[i] == 0) // p[1] = 0
+            {
+                p[i] = i; // p[1] = 1
+                i += 1; // i = 2
+            }
+        }
+
+        var pBatch = new int[n + 1];
+        for (var j = 0; j < p.Length; j++)
+            pBatch[j] = j;
+
+        int iBatch = nBatch;
+        while (iBatch < n)
+        {
+            pBatch[iBatch] -= 1;
+            var j = 0;
+            if (iBatch % 2 == 1) j = pBatch[iBatch];
+            SwapIntArray(elementsToPermute, iBatch, j);
+            result.Add(elementsToPermute.ToArray());
+            iBatch = 1;
+            while (pBatch[iBatch] == 0)
+            {
+                pBatch[iBatch] = iBatch;
+                iBatch += 1;
+            }
+        }
+
+        return result;
+    }
+
 
     public static void Swap(List<int> array, int i, int j)
     {
--- Recursion/PermutationTest/UnitTest1.cs
+++ Recursion/PermutationTest/UnitTest1.cs
@@ -5,71 +5,112 @@ namespace PermutationTest;
 
 public class UnitTest1
 {
+    private readonly List<List<int>> expectedOutputFor1234 = new()
+    {
+        new List<int> { 1, 2, 3, 4 },
+        new List<int> { 1, 2, 4, 3 },
+        new List<int> { 1, 3, 2, 4 },
+        new List<int> { 1, 3, 4, 2 },
+        new List<int> { 1, 4, 2, 3 },
+        new List<int> { 1, 4, 3, 2 },
+        new List<int> { 2, 1, 3, 4 },
+        new List<int> { 2, 1, 4, 3 },
+        new List<int> { 2, 3, 1, 4 },
+        new List<int> { 2, 3, 4, 1 },
+        new List<int> { 2, 4, 1, 3 },
+        new List<int> { 2, 4, 3, 1 },
+        new List<int> { 3, 1, 2, 4 },
+        new List<int> { 3, 1, 4, 2 },
+        new List<int> { 3, 2, 1, 4 },
+        new List<int> { 3, 2, 4, 1 },
+        new List<int> { 3, 4, 1, 2 },
+        new List<int> { 3, 4, 2, 1 },
+        new List<int> { 4, 1, 2, 3 },
+        new List<int> { 4, 1, 3, 2 },
+        new List<int> { 4, 2, 1, 3 },
+        new List<int> { 4, 2, 3, 1 },
+        new List<int> { 4, 3, 1, 2 },
+        new List<int> { 4, 3, 2, 1 }
+    };
+
+    private readonly List<int[]> expectedOutputFor1234Array = new()
+    {
+        new[] { 1, 2, 3, 4 },
+        new[] { 1, 2, 4, 3 },
+        new[] { 1, 3, 2, 4 },
+        new[] { 1, 3, 4, 2 },
+        new[] { 1, 4, 2, 3 },
+        new[] { 1, 4, 3, 2 },
+        new[] { 2, 1, 3, 4 },
+        new[] { 2, 1, 4, 3 },
+        new[] { 2, 3, 1, 4 },
+        new[] { 2, 3, 4, 1 },
+        new[] { 2, 4, 1, 3 },
+        new[] { 2, 4, 3, 1 },
+        new[] { 3, 1, 2, 4 },
+        new[] { 3, 1, 4, 2 },
+        new[] { 3, 2, 1, 4 },
+        new[] { 3, 2, 4, 1 },
+        new[] { 3, 4, 1, 2 },
+        new[] { 3, 4, 2, 1 },
+        new[] { 4, 1, 2, 3 },
+        new[] { 4, 1, 3, 2 },
+        new[] { 4, 2, 1, 3 },
+        new[] { 4, 2, 3, 1 },
+        new[] { 4, 3, 1, 2 },
+        new[] { 4, 3, 2, 1 }
+    };
+
+    private List<List<int>> expectedOutputFor123 = new()
+    {
+        new List<int> { 1, 2, 3 },
+        new List<int> { 1, 3, 2 },
+        new List<int> { 2, 1, 3 },
+        new List<int> { 2, 3, 1 },
+        new List<int> { 3, 2, 1 },
+        new List<int> { 3, 1, 2 }
+    };
+
+
     [Fact]
     public void Test1()
     {
-        List<int> input1 = new() { 1, 2, 3 };
+        List<int> input1 = new() { 1, 2, 3, 4 };
         List<List<int>> output1 = Recursive.GetPermutations(input1);
-        List<List<int>> expectedOutput = new()
-        {
-            new List<int> { 1, 2, 3 },
-            new List<int> { 1, 3, 2 },
-            new List<int> { 2, 1, 3 },
-            new List<int> { 2, 3, 1 },
-            new List<int> { 3, 2, 1 },
-            new List<int> { 3, 1, 2 }
-        };
-        output1.ShouldBe(expectedOutput, true);
+        output1.ShouldBe(expectedOutputFor1234, true);
     }
 
     [Fact]
     public void TestQuickPerm()
     {
-        List<int> input1 = new() { 1, 2, 3 };
+        List<int> input1 = new() { 1, 2, 3, 4 };
         List<List<int>> output1 = QuickPerm.GetPermutations(input1);
-        List<List<int>> expectedOutput = new()
-        {
-            new List<int> { 1, 2, 3 },
-            new List<int> { 1, 3, 2 },
-            new List<int> { 2, 1, 3 },
-            new List<int> { 2, 3, 1 },
-            new List<int> { 3, 2, 1 },
-            new List<int> { 3, 1, 2 }
-        };
-        output1.ShouldBe(expectedOutput, true);
+        output1.ShouldBe(expectedOutputFor1234, true);
     }
 
     [Fact]
-    public void TestQuickPermSwapRef()
+    public void TestQuickPermMultiCores()
     {
-        List<int> input1 = new() { 1, 2, 3 };
-        List<List<int>> output1 = QuickPerm.GetPermutations(input1);
-        List<List<int>> expectedOutput = new()
-        {
-            new List<int> { 1, 2, 3 },
-            new List<int> { 1, 3, 2 },
-            new List<int> { 2, 1, 3 },
-            new List<int> { 2, 3, 1 },
-            new List<int> { 3, 2, 1 },
-            new List<int> { 3, 1, 2 }
-        };
-        output1.ShouldBe(expectedOutput, true);
+        List<int> input1 = new() { 1, 2, 3, 4 };
+        List<int[]> output1 = QuickPerm.GetPermutationsIntArrayMultiCores(input1.ToArray());
+        output1.ShouldBe(expectedOutputFor1234Array, true);
     }
 
     [Fact]
-    public void TestQuickPerm10Items()
+    public void TestCompareQuickPermWithMultiCoresOdd()
     {
-        List<int> input1 = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-        List<List<int>> output1 = QuickPerm.GetPermutations(input1);
-        List<List<int>> expectedOutput = new()
-        {
-            new List<int> { 1, 2, 3 },
-            new List<int> { 1, 3, 2 },
-            new List<int> { 2, 1, 3 },
-            new List<int> { 2, 3, 1 },
-            new List<int> { 3, 2, 1 },
-            new List<int> { 3, 1, 2 }
-        };
-        output1.ShouldBe(expectedOutput, true);
+        List<int> input1 = new() { 1, 2, 3, 4, 5, 6, 7 };
+        List<int[]> outputMultiCores = QuickPerm.GetPermutationsIntArrayMultiCores(input1.ToArray());
+        List<int[]> outputSingleCore = QuickPerm.GetPermutationsIntArray(input1.ToArray());
+        outputSingleCore.ShouldBe(outputMultiCores, true);
+    }
+
+    [Fact]
+    public void TestCompareQuickPermWithMultiCoresEven()
+    {
+        List<int> input1 = new() { 1, 2, 3, 4, 5, 6 };
+        List<int[]> outputMultiCores = QuickPerm.GetPermutationsIntArrayMultiCores(input1.ToArray());
+        List<int[]> outputSingleCore = QuickPerm.GetPermutationsIntArray(input1.ToArray());
+        outputSingleCore.ShouldBe(outputMultiCores, true);
     }
 }
